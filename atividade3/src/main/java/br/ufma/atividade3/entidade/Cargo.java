302dfa958d78443fc19aee783fe91a9a59203317
Eduarda Maria de Albuquerque Chagas <eduarda.chagas@macbook-will0819.local>
2022-05-15 02:50:55 -0300

    mapeamento cargo

---
+++ atividade3/src/main/java/br/ufma/atividade3/entidade/Cargo.java
@@ -0,0 +1,34 @@
+package br.ufma.atividade3.entidade;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "cargo")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Cargo {
+
+    @Id
+    @Column(name="id_cargo")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="nome")
+    private String nome;
+
+    @Column(name="descricao")
+    private String descricao;
+}
