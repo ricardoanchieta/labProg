b2689401176ccb47cdbb3abe73f7fb523ac86a01
Eduarda Maria de Albuquerque Chagas <eduarda.chagas@macbook-will0819.local>
2022-05-15 19:29:32 -0300

    testes contato

---
+++ atividade3/src/test/java/br/ufma/atividade3/entidade/ContatoRepositoryTest.java
@@ -0,0 +1,92 @@
+package br.ufma.atividade3.entidade;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.ufma.atividade3.entidade.repositorio.ContatoRepo;
+
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@SpringBootTest
+@Transactional // o Transactional não persiste dados em teste, faz rollback após a conclusão deles
+public class ContatoRepositoryTest {
+
+    @Autowired
+    ContatoRepo repository;
+    @Test
+    public void deveSalvarContato() throws Exception {
+      
+      //cenário
+      Contato contato = Contato.builder()
+          .nome("Zap")
+          .urlLogo("canva.com")
+          .build();
+
+      //ação
+      Contato retorno = repository.save(contato);  //salva?
+      
+      //verificação
+      Assertions.assertNotNull(retorno);
+      Assertions.assertEquals(contato.getNome(), retorno.getNome());
+      Assertions.assertEquals(contato.getUrlLogo(), retorno.getUrlLogo());
+
+      //rollback
+      repository.delete(retorno);
+    }
+
+
+    @Test
+    public void deveBuscarContato() throws Exception {
+      
+      //cenário
+      Contato contato = Contato.builder()
+          .nome("Zap")
+          .urlLogo("canva.com")
+          .build();
+
+      //ação
+      Contato retorno = repository.save(contato);
+      List <Contato>  retList = repository.findByNome(contato.getNome());
+      
+      //verificação
+      Assertions.assertFalse(retList.isEmpty());
+
+      Contato ret = retList.get(0);
+
+      Assertions.assertEquals(contato.getNome(), ret.getNome());
+      Assertions.assertEquals(contato.getUrlLogo(), ret.getUrlLogo());
+
+      //rollback
+      repository.delete(retorno);
+    }
+
+    @Test
+    public void deveVerificarSeExisteContato() throws Exception {
+      
+      //cenário
+      Contato contato = Contato.builder()
+          .nome("Zap")
+          .urlLogo("canva.com")
+          .build();
+
+      //ação
+       
+      Contato retorno = repository.save(contato);  //salva?
+      boolean  ret = repository.existsByNome(contato.getNome());
+      
+      //verificação
+      Assertions.assertTrue(ret);
+
+      //rollback
+      repository.delete(retorno);
+    }
+}
