7a5871a94cd3ddfdef7965a38637fd9dca75e9e6
Pedro Bernhard <pvbernhard@gmail.com>
2022-06-10 23:09:52 -0300

    add: egresso service

---
+++ atividade3/src/main/java/br/ufma/atividade3/service/EgressoService.java
@@ -0,0 +1,45 @@
+package br.ufma.atividade3.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.ufma.atividade3.entidade.Egresso;
+import br.ufma.atividade3.entidade.repositorio.EgressoRepo;
+import br.ufma.atividade3.service.exceptions.ErroDepoimentoRunTime;
+import br.ufma.atividade3.service.exceptions.ErroEgressoRunTime;
+
+@Service
+public class EgressoService {
+  
+  @Autowired
+  EgressoRepo repository;
+
+  public Egresso salvarEgresso(Egresso egresso) {
+    verificarEgresso(egresso);
+    return repository.save(egresso);
+  }
+
+  public void deletarEgresso(Long id) {
+    Optional<Egresso> egresso = repository.findById(id);
+    if (!egresso.isPresent())
+      throw new ErroEgressoRunTime("Egresso inexistente");
+    repository.delete(egresso.get());
+  }
+
+  private void verificarId(Egresso egresso) {
+    if ((egresso == null) || (egresso.getId() == null))
+      throw new ErroEgressoRunTime("Egresso inválido");
+  }
+
+  private void verificarEgresso(Egresso egresso) {
+    verificarId(egresso);
+    if ((egresso.getNome() == null) || (egresso.getNome().equals("")))
+      throw new ErroEgressoRunTime("Nome deve ser informado");
+    if ((egresso.getCpf() == null) || (egresso.getCpf().equals("")))
+        throw new ErroDepoimentoRunTime("CPF deve ser informado");
+    if ((egresso.getEmail() == null) || (egresso.getEmail().equals("")))
+        throw new ErroDepoimentoRunTime("Email deve ser informado");
+  }
+}
---
+++ atividade3/src/main/java/br/ufma/atividade3/service/exceptions/ErroEgressoRunTime.java
@@ -0,0 +1,9 @@
+package br.ufma.atividade3.service.exceptions;
+
+public class ErroEgressoRunTime extends RuntimeException {
+
+  public ErroEgressoRunTime(String msg) {
+    super(msg);
+  }
+  
+}
