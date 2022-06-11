004296e4909bad960b78d977cb89d1946559db55
Pedro Bernhard <pvbernhard@gmail.com>
2022-06-10 23:10:42 -0300

    edit: prof egresso

--- atividade3/src/main/java/br/ufma/atividade3/entidade/ProfEgresso.java
+++ atividade3/src/main/java/br/ufma/atividade3/entidade/ProfEgresso.java
@@ -3,7 +3,6 @@ package br.ufma.atividade3.entidade;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
-import org.springframework.data.convert.Jsr310Converters;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 import javax.persistence.*;
