fc580f0aff87de0bdf64252e3972785e2e94ee4f
EduardaAChagas <eduardamachagas@gmail.com>
2022-06-21 19:48:41 -0300

    Correções nos testes de entidade

--- atividade3/src/test/java/br/ufma/atividade3/entidade/FaixaSalarioRepositoryTest.java
+++ atividade3/src/test/java/br/ufma/atividade3/entidade/FaixaSalarioRepositoryTest.java
@@ -6,35 +6,34 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.transaction.annotation.Transactional;
 
-import br.ufma.atividade3.entidade.FaixaSalario;
+import br.ufma.atividade3.entidade.repositorio.FaixaSalarioRepo;
 
 
 @ExtendWith(SpringExtension.class)
-@ActiveProfiles("test")
 @SpringBootTest
 public class FaixaSalarioRepositoryTest {
 
     @Autowired
-    FaixaSalarioRepositoryTest repository;
+    FaixaSalarioRepo repository;
     
     @Test
     public void deveSalvarFaixaSalario() {
       
       //cenário
-      FaixaSalario faixaSalario = FaixaSalario.builder().descricao("vinte mil reis").build();
+      FaixaSalario novafaixaSalario = FaixaSalario.builder().descricao("vinte mil reis").build();
       //ação
-      FaixaSalario retorno = repository.save(faixaSalario);
+      FaixaSalario retorno = repository.save(novafaixaSalario);
       
       //verificação
       Assertions.assertNotNull(retorno);
-      Assertions.assertEquals(faixaSalario.getDescricao(), retorno.getDescricao());
+      Assertions.assertEquals(novafaixaSalario.getDescricao(), retorno.getDescricao());
 
       //rollback
-      repository.delete(retorno);
+      //repository.delete(retorno);
     }
+
+
 
 }
