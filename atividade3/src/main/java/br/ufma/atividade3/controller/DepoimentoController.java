b9b4d65c6409cb9d303810646c9d59c19fca189e
Ricardo Anchieta <ricardoanchieta99@gmail.com>
2022-06-21 22:21:15 -0300

    Add services no controller de Depoimentos

--- atividade3/src/main/java/br/ufma/atividade3/controller/DepoimentoController.java
+++ atividade3/src/main/java/br/ufma/atividade3/controller/DepoimentoController.java
@@ -3,10 +3,12 @@ package br.ufma.atividade3.controller;
 import br.ufma.atividade3.entidade.Depoimento;
 import br.ufma.atividade3.entidade.Egresso;
 import br.ufma.atividade3.entidade.dto.DepoimentoDTO;
+import br.ufma.atividade3.entidade.dto.EgressoDTO;
 import br.ufma.atividade3.entidade.repositorio.DepoimentoRepo;
 import br.ufma.atividade3.entidade.repositorio.EgressoRepo;
 import br.ufma.atividade3.service.DepoimentoService;
 import br.ufma.atividade3.service.exceptions.ErroDepoimentoRunTime;
+import br.ufma.atividade3.service.exceptions.ErroEgressoRunTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Date;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/depoimento")
@@ -28,7 +32,7 @@ public class DepoimentoController {
     @Autowired
     EgressoRepo egressoRepo;
 
-    @PostMapping("salvar")
+    @PostMapping("/salvar")
     public ResponseEntity salvarDepoimento(@RequestBody DepoimentoDTO request){
         Egresso egresso = egressoRepo.getById(request.getId_egresso());
 
@@ -46,5 +50,30 @@ public class DepoimentoController {
         }
     }
 
+    @PostMapping("/delete")
+    public ResponseEntity deleteDepoimento(@RequestBody DepoimentoDTO request){
+        Long id = request.getId();
+
+        try{
+            service.deletarDepoimento(id);
+            return new ResponseEntity("Depoimento deletado com sucesso",HttpStatus.ACCEPTED);
+        }catch (ErroEgressoRunTime ex){
+            return ResponseEntity.badRequest().body(ex.getMessage());
+        }
+    }
+
+    @PostMapping("/listar")
+    public ResponseEntity listaDepoimentoPorEgresso(@RequestBody DepoimentoDTO request) {
+        Long id_egresso = request.getId_egresso();
+
+        Egresso egresso = egressoRepo.getById(id_egresso);
+
+        try{
+            List<Depoimento> depoimentos = service.depoimentosPorEgresso(egresso);
+            return new ResponseEntity(depoimentos,HttpStatus.ACCEPTED);
+        }catch (ErroEgressoRunTime ex){
+            return ResponseEntity.badRequest().body(ex.getMessage());
+        }
+    }
 
 }
